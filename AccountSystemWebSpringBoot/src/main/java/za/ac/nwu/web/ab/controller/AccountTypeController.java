00fccc14242562a26d3769944a0d519299475b49
HoneyBadger07 <tiaanlouw5@gmail.com>
2021-09-26 23:35:30 +0200

    Added project files

---
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/web/ab/controller/AccountTypeController.java
@@ -0,0 +1,11 @@
+package za.ac.nwu.web.ab.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountTypeController {
+
+
+
+}
